8f510b1095d06a7c062e253f07950fba770fd9f5
Wes Rockholz <wesley.rockholz@gmail.com>
2022-10-22 22:37:58 -0700

    Add a button that opens a copyable window with link to wowhead site

--- Source/main.lua
+++ Source/main.lua
@@ -1,28 +1,53 @@
 AchievementNotes.Chat:Print("Loaded main.lua")
+local data = {}
+
+StaticPopupDialogs["AchievementUrl"] = {
+    text = "Copy & Paste the link into your browser",
+    button1 = "Close",
+    OnAccept = function()
+    end,
+    timeout = 0,
+    whileDead = true,
+    hideOnEscape = true,
+    preferredIndex = 3,
+    OnShow =
+        function (self, data)
+            self.editBox:SetText(data.url)
+            self.editBox:SetWidth(self.editBox:GetParent():GetWidth() - 50)
+            self.editBox:HighlightText()
+        end,
+    hasEditBox = true
+}
 
 function AchievementNotes.OnEvent(self, event, arg1, ...)
     if (event == "ADDON_LOADED" and arg1 == "Blizzard_AchievementUI") then
         AchievementNotes.Chat:Print(arg1.." loaded")
         for i,button in ipairs(AchievementFrameAchievementsContainer.buttons) do
             button:HookScript("OnUpdate", AchievementNotes.OnAchievementButtonUpdate)
-            button:HookScript("OnClick", AchievementNotes.OnAchievementButtonClicked)
-            button.eb = CreateFrame("EditBox", "eb", button)
-            button.eb:SetMultiLine(true)
-            button.eb:SetAutoFocus(false)
-            button.eb:SetPoint("BOTTOM")
-            button.eb:SetSize(400, 200)
-            button.eb:SetFontObject("ChatFontNormal")
-            button.eb:Show()
+            button:HookScript("OnClick", AchievementNotes.OnAchievementButtonClicked)local
+            b = CreateFrame("Button", "MyButton", button, "UIPanelButtonTemplate")
+            b:SetSize(80, 14)
+            b:SetText("wowhead")
+            b:SetPoint("BOTTOMRIGHT")
+            b:SetScript("OnClick", function()
+                local d = {}
+                d.url = "https://www.wowhead.com/achievement=" .. button.id .. "/"
+                StaticPopup_Show("AchievementUrl", "", "", d)
+            end)
+            b:Show()
         end
     end
 end
 
 function AchievementNotes.OnAchievementButtonUpdate(self)
-    local _, achievementName, points, completed = GetAchievementInfo(self.id)
+    local id, achievementName, points, completed = GetAchievementInfo(self.id)
+    if data[id] then
+        self.eb:SetText(data[id])
+    end
 end
 
 function AchievementNotes.OnAchievementButtonClicked(self)
-    local _, achievementName = GetAchievementInfo(self.id)
+    local id, achievementName = GetAchievementInfo(self.id)
 end
 
 AchievementNotes.MainFrame = CreateFrame("Frame")
